8d4fd897222f6f7315fc8ea94398696e043f82ae
Ben Y <jyf0000@gmail.com>
2015-03-09 13:42:08 -0600

    Bug fix with permissions handling while masquerading

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -53,6 +53,7 @@ class ApplicationController < ActionController::Base
     if logged_in? && (cookie_type['sports_b_masquerade_user'] || params['as_user_id'])
       masq_user = User.active.find_by_id cookie_type['sports_b_masquerade_user'] || params['as_user_id']
       @current_user = masq_user if can?(:masquerade, masq_user)
+      @current_ability = Ability.new(@current_user)
     end
   end
 
